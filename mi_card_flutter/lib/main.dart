d177da9337d5ad6c581b089393c9060c43c2ba5e
ryhanlon <ryhanlon@gmail.com>
2020-06-17 13:37:46 -0700

    mi_card complete

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -11,95 +11,93 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.indigo,
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage(
-                  'images/reb_bluescarf@600px.jpg',
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage(
+                    'images/reb_bluescarf@600px.jpg',
+                  ),
                 ),
-              ),
-              Text(
-                'Rebecca Y. Hanlon',
-                style: TextStyle(
-                  fontFamily: 'Sacramento-Regular',
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+                Text(
+                  'Rebecca Y. Hanlon',
+                  style: TextStyle(
+                    fontFamily: 'Sacramento-Regular',
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              Text(
-                'WEB & FLUTTER',
-                style: TextStyle(
-                  fontFamily: 'JosefinSlab-Bold',
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.indigo.shade100,
+                Text(
+                  'WEB & FLUTTER',
+                  style: TextStyle(
+                    fontFamily: 'JosefinSlab-Bold',
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.indigo.shade100,
+                  ),
                 ),
-              ),
-              Text(
-                'DEVELOPER',
-                style: TextStyle(
-                  fontFamily: 'JosefinSlab-Bold',
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.indigo.shade100,
+                Text(
+                  'DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'JosefinSlab-Bold',
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.indigo.shade100,
+                  ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.indigo,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 15.0,
+                    width: 230.0,
+                    child: Divider(
+                      color: Colors.purple.shade100,
                     ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      '702-985-1402',
-                      style: TextStyle(
-                        color: Colors.indigo.shade900,
-                        fontFamily: 'JosefinSlab-Regular',
-                        fontSize: 20.0,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.indigo,
                       ),
-                    )
-                  ],
+                      title: Text(
+                        '702-985-1402',
+                        style: TextStyle(
+                          color: Colors.indigo.shade900,
+                          fontFamily: 'JosefinSlab-Regular',
+                          fontSize: 20.0,
+                        ),
+                      )),
                 ),
-              ),
-              Container(
-                color: Colors.white,
-                padding: EdgeInsets.all(10.0),
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
                       Icons.email,
                       color: Colors.indigo,
                     ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
+                    title: Text(
                       'ryhanlon@gmail.com',
                       style: TextStyle(
                           fontFamily: 'JosefinSlab-Regular',
-                          fontSize: 20.0,
+                          fontSize: 18.0,
                           color: Colors.indigo.shade900),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
