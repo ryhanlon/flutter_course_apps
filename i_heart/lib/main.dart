4be08d093cec182ffbe262445745c5c74ba6828a
ryhanlon <ryhanlon@gmail.com>
2020-07-07 20:42:57 -0700

    feat: add Dicee app

--- i_heart/lib/main.dart
+++ i_heart/lib/main.dart
@@ -1,21 +1,21 @@
-//import 'package:flutter/material.dart';
-//
-//// The main function is starting point for app.
-//void main() {
-//  runApp(
-//    MaterialApp(
-//      home: Scaffold(
-//        backgroundColor: Color.fromARGB(255, 129, 165, 245),
-//        appBar: AppBar(
-//          title: Text('I Heart You'),
-//          backgroundColor: Colors.black38,
-//        ),
-//        body: Center(
-//          child: Image(
-//            image: AssetImage('images/heart.png'),
-//          ),
-//        ),
-//      ),
-//    ),
-//  );
-//}
+import 'package:flutter/material.dart';
+
+// The main function is starting point for app.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 129, 165, 245),
+        appBar: AppBar(
+          title: Text('I Heart You'),
+          backgroundColor: Colors.black38,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/heart.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
