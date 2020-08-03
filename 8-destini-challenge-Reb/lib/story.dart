15ed605721a9b056eacfa827cea3316bcfd6ec4d
ryhanlon <ryhanlon@gmail.com>
2020-08-02 23:33:41 -0700

    feat: Destini app completed

---
+++ 8-destini-challenge-Reb/lib/story.dart
@@ -0,0 +1,10 @@
+//Create a new class called Story.
+class Story {
+//Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+// Constructor for to initialise the properties.
+  Story({this.storyTitle, this.choice1, this.choice2});
+}
