2e627b1b1f419a77bf3f6f60452e6eaf0516d9d4
ryhanlon <ryhanlon@gmail.com>
2021-02-22 21:59:03 -0800

    feat: my card reb, add column

--- 3-mi_card_reb/lib/main.dart
+++ 3-mi_card_reb/lib/main.dart
@@ -11,13 +11,35 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.deepPurple,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.indigoAccent,
-            child: Text('hello'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.indigoAccent,
+                child: Text('container 1'),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.purple,
+                child: Text('container 2'),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.amber,
+                child: Text('container 3'),
+              ),
+            ],
           ),
         ),
       ),
